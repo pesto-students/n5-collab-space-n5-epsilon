628ef874bc16b75e371554dc0619b5664439fc75
devansh332 <devanshsaini022@gmail.com>
2021-08-10 11:38:58 +0530

    validation for all CRUD optionation endpoints

---
+++ src/server/Validations/userSchemaValidations copy.jsx
@@ -0,0 +1,20 @@
+const validation = require('@hapi/joi');
+
+const signUp_UserValidation = (data) => {
+  const signUp_User = {
+    name: validation.string().min(4).required(),
+    email: validation.string().min(5).required().email(),
+    password: validation.string().min(6).required(),
+  };
+  return validation.validate(data, signUp_User);
+};
+
+const login_UserValidation = (data) => {
+  const login_User = {
+    email: validation.string().min(5).required().email(),
+    password: validation.string().min(6).required(),
+  };
+  return validation.validate(data, login_User);
+};
+
+export { signUp_UserValidation, login_UserValidation };
