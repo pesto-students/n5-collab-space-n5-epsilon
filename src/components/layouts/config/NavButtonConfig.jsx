dfcf3673514caaff13fe7fde1756f325fd124324
devansh332 <devanshsaini022@gmail.com>
2021-08-12 23:03:13 +0530

    added icon from svg

--- src/components/layouts/config/NavButtonConfig.jsx
+++ src/components/layouts/config/NavButtonConfig.jsx
@@ -1,23 +1,24 @@
 import React from "react";
+import ProjectIcon from "../iconComponents/ProjectIcon";
+import SettingIcon from "../iconComponents/SettingIcon";
+import UserIcon from "../iconComponents/UserIcon";
 const navButtons = [
   {
     label: "Projects",
     path: "/workspace",
-    icon: "+",
+    icon: <ProjectIcon height="50pt" width="50pt" />,
     child: {},
   },
   {
     label: "User Panel",
     path: "/workspace/user-panel",
-    icon: "()",
+    icon: <UserIcon />,
   },
   {
     label: "setting",
     path: "/workspace/setting",
-    icon: "{}",
+    icon: <SettingIcon />,
   },
 ];
 
 export default navButtons;
-
-// remove co
