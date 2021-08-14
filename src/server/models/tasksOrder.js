f6e758946dbe74bde2ee2afef6b3b4b0d46e528d
devansh332 <devanshsaini022@gmail.com>
2021-08-14 14:55:09 +0530

    added taskListsOrder and tasksOrder

---
+++ src/server/models/tasksOrder.js
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from "mongoose";
+
+const MODEL_NAME = "TasksOrder";
+
+const TasksOrderSchema = new Schema({
+  taskListId: {
+    type: Schema.Types.ObjectId,
+    ref: "TaskLists",
+  },
+  tasksOrder: [Schema.Types.ObjectId],
+});
+
+const Model =
+  mongoose.models[MODEL_NAME] ||
+  mongoose.model(MODEL_NAME, TasksOrderSchema, "tasks.order");
+
+export default Model;
