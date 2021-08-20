baaf81f00ed1a53cf9c6d84df2b2d5057b1539a5
Hbhatia96 <himanshubhatia1996@gmail.com>
2021-08-21 01:26:37 +0530

    endpoint fix check 2

--- src/client_apis/workSpaceApi.js
+++ src/client_apis/workSpaceApi.js
@@ -3,17 +3,17 @@ import axios from "axios";
 export const urlBackEnd = process.env.REACT_APP_BACK_END_URL;
 
 export const usersURL = axios.create({
-  baseURL: `${urlBackEnd}/api/users`,
+  baseURL: `/api/users`,
 });
 
 export const projectURL = axios.create({
-  baseURL: `${urlBackEnd}/api/projects`,
+  baseURL: `/api/projects`,
 });
 
 export const taskListURL = axios.create({
-  baseURL: `${urlBackEnd}/api/taskList`,
+  baseURL: `/api/taskList`,
 });
 
 export const taskURL = axios.create({
-  baseURL: `${urlBackEnd}/api/task`,
+  baseURL: `/api/task`,
 });
