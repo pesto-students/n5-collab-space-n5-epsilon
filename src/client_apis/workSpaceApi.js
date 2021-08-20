ce2d978767a89bc3b45c59d5aa541729ce0caa8b
Hbhatia96 <himanshubhatia1996@gmail.com>
2021-08-21 02:00:52 +0530

    endpoints check 3

--- src/client_apis/workSpaceApi.js
+++ src/client_apis/workSpaceApi.js
@@ -1,19 +1,19 @@
-import axios from "axios";
+import Axios from "axios";
 
-export const urlBackEnd = process.env.REACT_APP_BACK_END_URL;
+export const urlBackEnd = process.env.BACK_END_URL;
 
-export const usersURL = axios.create({
-  baseURL: `/api/users`,
+export const usersURL = Axios.create({
+  baseURL: `${urlBackEnd}/api/users`,
 });
 
-export const projectURL = axios.create({
-  baseURL: `/api/projects`,
+export const projectURL = Axios.create({
+  baseURL: `${urlBackEnd}/api/projects`,
 });
 
-export const taskListURL = axios.create({
-  baseURL: `/api/taskList`,
+export const taskListURL = Axios.create({
+  baseURL: `${urlBackEnd}/api/taskList`,
 });
 
-export const taskURL = axios.create({
-  baseURL: `/api/task`,
+export const taskURL = Axios.create({
+  baseURL: `${urlBackEnd}/api/task`,
 });
