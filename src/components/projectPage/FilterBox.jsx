9aadb6a57526ea35e2c111f5e2c589ea70846131
devansh332 <devanshsaini022@gmail.com>
2021-08-22 18:45:44 +0530

    added new files

---
+++ src/components/projectPage/FilterBox.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+function FilterBox({ taskListsName, taskTagNames }) {
+  return (
+    <div className="filter-box">
+      <div className="filter-wrapper">
+        <p>Task Type</p>
+        <select onChange={(e) => setTaskTypeFilter(e.target.value)}>
+          <option value="any">Any</option>
+          {taskListsName.length &&
+            taskListsName.map((taskListName) => {
+              return (
+                <option key={taskListName} value={taskListName}>
+                  {taskListName}
+                </option>
+              );
+            })}
+        </select>
+      </div>
+
+      <div className="filter-wrapper">
+        <p>Tag Type</p>
+        <select onChange={(e) => setTaskTagFilter(e.target.value)}>
+          <option value="any">Any</option>
+          {Object.keys(taskTagNames).length &&
+            Object.keys(taskTagNames).map((tagName) => {
+              return (
+                <option key={tagName} value={tagName}>
+                  {tagName}
+                </option>
+              );
+            })}
+        </select>
+      </div>
+    </div>
+  );
+}
+
+export default FilterBox;
